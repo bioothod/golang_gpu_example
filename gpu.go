8d100fb36f5d278788ec38cfa470377fe67f8bad
Evgeniy Polyakov <zbr@ioremap.net>
2018-07-12 15:36:46 +0100

    Initial import

---
+++ gpu.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	tensorflow_config "github.com/tensorflow/tensorflow/tensorflow/go/core/protobuf"
+	
+	"github.com/gogo/protobuf/proto"
+
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func create_graph(model_path string) (*tf.Graph) {
+	model, err := ioutil.ReadFile(model_path)
+	if err != nil {
+		log.Fatalf("could not read model from %s: %v", model_path, err)
+	}
+
+	graph := tf.NewGraph()
+	if err := graph.Import(model, ""); err != nil {
+		log.Fatalf("could not load model from %s: %v", model_path, err)
+	}
+
+	return graph
+}
+
+func session_config() *tf.SessionOptions {
+       config := &tensorflow_config.ConfigProto {
+	       LogDevicePlacement: true,
+	       AllowSoftPlacement: false,
+	       GpuOptions: &tensorflow_config.GPUOptions {
+	       },
+       }
+       ser, err := proto.Marshal(config)
+       if err != nil {
+               log.Fatalf("could not serialize config: %v", err)
+       }
+
+       return &tf.SessionOptions {
+               Config: ser,
+       }
+}
+
+func new_session(graph *tf.Graph) *tf.Session {
+	so := session_config()
+	sess, err := tf.NewSession(graph, so)
+	if err != nil {
+		log.Fatalf("could not create session: %v", err)
+	}
+	return sess
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		log.Fatalf("path to serialized graph must be provioded")
+	}
+
+	graph := create_graph(args[0])
+
+	session := new_session(graph)
+
+	i0, err := tf.NewTensor([][]int32{{1, 2, 3}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	i1, err := tf.NewTensor([][]int32{{3, 4, 5}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := session.Run(
+		map[tf.Output]*tf.Tensor{
+			graph.Operation("input/ph0").Output(0): i0,
+			graph.Operation("input/ph1").Output(0): i1,
+		},
+
+		[]tf.Output{
+			graph.Operation("output/op").Output(0),
+		},
+
+		nil)
+
+	if err != nil {
+		log.Fatalf("could not run face detection inference: %v", err)
+	}
+
+	output := res[0].Value().([][]int32)
+	fmt.Printf("op: %v\n", output)
+}
---
+++ make_graph.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output', type=str, required=True, help='Output path for frozen protobuf file')
+
+def main(args=None):
+    with tf.Graph().as_default() as g:
+        ph0 = tf.placeholder(tf.int32, [None, 3], name='input/ph0')
+        ph1 = tf.placeholder(tf.int32, [None, 3], name='input/ph1')
+
+        op = tf.matmul(ph0, ph1, transpose_a=True, name='output/op')
+
+        with tf.Session(graph=g) as sess:
+            sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+
+            output_graph_def = tf.graph_util.convert_variables_to_constants(sess, g.as_graph_def(), ['output/op'])
+
+            try:
+                with tf.gfile.GFile(FLAGS.output, "wb+") as f:
+                    f.write(output_graph_def.SerializeToString())
+            except Exception as e:
+                print('Could not save graph to {}: {}'.format(FLAGS.output, e))
+                return
+
+if __name__ == '__main__':
+    FLAGS = parser.parse_args()
+
+    tf.app.run()
